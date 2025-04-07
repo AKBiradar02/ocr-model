603671664628875cdb7bc2ec1179e3b486710038
Biradar Abhay Kapil <1032211780@mitwpu.edu.in>
2025-04-07 12:22:28 +0530

    Delete frontend directory

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders OCR Text Extractor heading', () => {
-    render(<App />);
-    const headingElement = screen.getByText(/OCR Text Extractor/i);
-    expect(headingElement).toBeInTheDocument();
-});
